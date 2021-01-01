30b001abbacccfd43a2f8ddac0773da539b0e008
Enijar <TheEnijar@gmail.com>
2021-01-01 15:05:29 +0000

    chore: center image in canvas

--- src/components/canvas/styles.ts
+++ src/components/canvas/styles.ts
@@ -6,11 +6,14 @@ export const Wrapper = styled.div<WrapperProps>`
   overflow: hidden;
   position: relative;
   user-select: none;
+  display: flex;
 
   ${(props) => css`
     img {
       max-width: ${props.width}px;
       max-height: ${props.height}px;
+      align-self: center;
+      flex-shrink: 0;
     }
   `}
 `;
