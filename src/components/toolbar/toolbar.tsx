a1fbad232368f4ee18dc1b9b19feb125c647f44a
Enijar <TheEnijar@gmail.com>
2021-01-07 21:12:25 +0000

    fix: add alt prop to images

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -40,24 +40,24 @@ export default function Toolbar() {
         active={tool === Tool.move}
         title="Move (v)"
       >
-        <img src={MOVE_ICON} />
+        <img src={MOVE_ICON} alt="" />
       </ToolButton>
       <ToolButton
         onClick={toggleTool(Tool.zoom)}
         active={tool === Tool.zoom}
         title="Zoom (z)"
       >
-        <img src={ZOOM_ICON} />
+        <img src={ZOOM_ICON} alt="" />
       </ToolButton>
       <ToolButton
         onClick={toggleTool(Tool.brush)}
         active={tool === Tool.brush}
         title="Brush (b)"
       >
-        <img src={BRUSH_ICON} />
+        <img src={BRUSH_ICON} alt="" />
       </ToolButton>
       <ToolButton inactive title="Color">
-        <img src={COLOR_ICON(color)} />
+        <img src={COLOR_ICON(color)} alt="" />
         <input
           ref={colorInput}
           type="color"
